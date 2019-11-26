fa5ecae1abc33b0c96ec4b56f2e035d7f5be4216
DrewFrost <drew.vfrost@gmail.com>
2019-11-26 17:46:49 +0200

    init postgres

--- main.go
+++ main.go
@@ -1,7 +1,66 @@
 package main
 
-import "regexp"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"regexp"
 
+	_ "github.com/lib/pq"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	dbname = "number_normalizer "
+)
+
+var (
+	user     = os.Getenv("POSTGRES_USER")
+	password = os.Getenv("POSTGRES_PASS")
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+	err = resetDB(db, dbname)
+	must(err)
+	db.Close()
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	err = db.Ping()
+	must(err)
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func resetDB(db *sql.DB, name string) error {
+	qr := fmt.Sprintf("DROP DATABASE IF EXISTS %s", dbname)
+	_, err := db.Exec(qr)
+	if err != nil {
+		return err
+	}
+	return createDB(db, name)
+}
+
+func createDB(db *sql.DB, name string) error {
+	qr := fmt.Sprintf("CREATE DATABASE %s", dbname)
+	_, err := db.Exec(qr)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//Regex
 func normalize(phone string) string {
 	re := regexp.MustCompile("[^0-9]")
 	return re.ReplaceAllString(phone, "")
@@ -17,6 +76,3 @@ func normalize(phone string) string {
 // 	}
 // 	return buf.String()
 // }
-
-func main() {
-}
