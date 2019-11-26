fedfc2a48d00184eef83ab00c15ca010f1387a69
DrewFrost <drew.vfrost@gmail.com>
2019-11-26 14:25:55 +0200

    regex normalize

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
-import "bytes"
+import "regexp"
 
 func normalize(phone string) string {
-	var buf bytes.Buffer
-	for _, ch := range phone {
-		if ch >= '0' && ch <= '9' {
-			buf.WriteRune(ch)
-		}
-	}
-	return buf.String()
+	re := regexp.MustCompile("[^0-9]")
+	return re.ReplaceAllString(phone, "")
 }
+
+// Normaliza Without Regex
+// func normalize(phone string) string {
+// 	var buf bytes.Buffer
+// 	for _, ch := range phone {
+// 		if ch >= '1' && ch <= '9' {
+// 			buf.WriteRune(ch)
+// 		}
+// 	}
+// 	return buf.String()
+// }
 
 func main() {
 }
