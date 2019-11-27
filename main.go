f2453e5563d7c4c4bae08ee1928557a26a26dfd0
DrewFrost <drew.vfrost@gmail.com>
2019-11-27 20:19:26 +0200

    get all phones

--- main.go
+++ main.go
@@ -29,24 +29,57 @@ func init() {
 }
 
 func main() {
+	// initDB()
+	// numbers := []string{"1234567890", "123 456 7891", "(123) 456 7892", "(123) 456-7893", "123-456-7894", "123-456-7890", "1234567892", "(123)456-7892"}
+	db, err := connectToExistingDB()
+	must(err)
+	defer db.Close()
+	// must(createPhoneTable(db))
+	// for i, v := range numbers {
+	// 	id, err := insertPhone(db, v)
+	// 	must(err)
+	// 	ids[i] = id
+	// }
+	phones, err := getAllPhones(db)
+	must(err)
+	for _, p := range phones {
+		fmt.Printf("%v\n", p)
+	}
+
+}
+
+func initDB() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
 	db, err := sql.Open("postgres", psqlInfo)
 	must(err)
 	err = resetDB(db, dbname)
 	must(err)
 	db.Close()
-	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
-	db, err = sql.Open("postgres", psqlInfo)
-	must(err)
-	defer db.Close()
-
-	must(createPhoneTable(db))
 }
 
-func must(err error) {
+func connectToExistingDB() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+	return db, nil
+}
+
+func createDB(db *sql.DB, name string) error {
+	qr := fmt.Sprintf("CREATE DATABASE %s", dbname)
+	_, err := db.Exec(qr)
 	if err != nil {
-		panic(err)
+		return err
 	}
+	return nil
+}
+
+func resetDB(db *sql.DB, name string) error {
+	qr := fmt.Sprintf("DROP DATABASE IF EXISTS %s", dbname)
+	_, err := db.Exec(qr)
+	if err != nil {
+		return err
+	}
+	return createDB(db, name)
 }
 
 func createPhoneTable(db *sql.DB) error {
@@ -59,22 +92,49 @@ func createPhoneTable(db *sql.DB) error {
 	return err
 }
 
-func resetDB(db *sql.DB, name string) error {
-	qr := fmt.Sprintf("DROP DATABASE IF EXISTS %s", dbname)
-	_, err := db.Exec(qr)
+func insertPhone(db *sql.DB, phone string) (int, error) {
+	var id int
+	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+	err := db.QueryRow(statement, phone).Scan(&id)
 	if err != nil {
-		return err
+		return -1, err
 	}
-	return createDB(db, name)
+	return id, nil
 }
 
-func createDB(db *sql.DB, name string) error {
-	qr := fmt.Sprintf("CREATE DATABASE %s", dbname)
-	_, err := db.Exec(qr)
+func getPhone(db *sql.DB, id int) (string, error) {
+	var phone string
+	err := db.QueryRow("SELECT value FROM phone_numbers WHERE id=$1", id).Scan(&phone)
 	if err != nil {
-		return err
+		return "", err
 	}
-	return nil
+	return phone, nil
+}
+
+func getAllPhones(db *sql.DB) ([]string, error) {
+	rows, err := db.Query("SELECT value FROM phone_numbers")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var ret []string
+	for rows.Next() {
+		var phone string
+		if err := rows.Scan(&phone); err != nil {
+			return nil, err
+		}
+		ret = append(ret, phone)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 //Regex
